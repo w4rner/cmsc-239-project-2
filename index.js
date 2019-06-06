27d940cbfe14dac2386e430a8f0b4e0573545d91
Jack <jack.k.lundell@gmail.com>
2019-06-06 16:56:08 -0500

    pushing before leaving; changing tags in index.html

--- index.js
+++ index.js
@@ -1,41 +1,10 @@
+import {first, second, third} from ./src/utils.js
+
 document.addEventListener('DOMContentLoaded', () => {
-  function first() {
-      console.log('first running');
-      let xs = []
-
-      for(var i = 0; i < 200 * Math.PI; i+= .01){
-          xs.push(i);
-      }
-
-      let t = 0;
-
-      let A = 20;
-      let f = 1/10;
-      let height = 200;
-
-      $("#slider").on("input", function() {
-          A = $(this).val();
-      });
-
-      function animate(){
-          let points = xs.map(x => {
-              let y = A * Math.sin((x + t) * f);
-              return[x,y + height];
-          });
-
-          let path = "M" + points.map(p => {
-              return p[0] + "," + p[1];
-          }).join('L')
-
-          document.querySelector("#first").setAttribute("d", path);
-
-          t += 1;
-          requestAnimationFrame(animate);
-
-      };
-      animate();
-  }
-
+  first();
+  second();
+  third();
+}, false);
   const text1 = `
     Sound can be thought of as oscillations in the density of air particles. These oscillations
   can be modeled as waves, with a given beats per second, or frequency, and a volume
@@ -78,4 +47,3 @@ document.addEventListener('DOMContentLoaded', () => {
     .attr('id', "slider")
 
   first();
-}, false);
