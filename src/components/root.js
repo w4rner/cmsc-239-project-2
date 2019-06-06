8c5c4631cb770f7f945b2eaa2079c363e646ffc7
Jack <jack.k.lundell@gmail.com>
2019-06-06 11:06:27 -0500

    Ok, last one didn't quite go through I think? Now adding DFT and inverseDFT as well as styling changes

--- src/components/root.js
+++ src/components/root.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {csv} from 'd3-fetch';
 import ExampleChart from './example-chart';
+import {DFT} from '../utils.js'
 
 const longBlock = `
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
@@ -9,6 +10,35 @@ laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in repreh
 voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
 non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 `;
+
+/*const paragraph1 = `
+  Consider a monotone sound. The number of beats per second, or the frequency, of the sound is 
+constant. One can represent this pure tone as a wave function where the height of the maximum or
+minimum point of the wave represents the volume of the sound, called the amplitude.
+`;*/
+const paragraph1 = `
+  Sound can be thought of as oscillations in the density of air particles. These oscillations
+can be modeled as waves, with a given beats per second, or frequency, and a volume
+given by the height, or amplitude of the wave.
+`;
+
+const paragraph2 = `
+When two waves "packets" meet, their values add linearly.
+`;
+
+const paragraph3 = `
+The same principle holds for any number of sound waves, but distinguishing between the contributions
+of sounds with different frequencies becomes difficult,`
+
+const paragraph4 = `
+So, we ask, given an arbitrary sound wave, how can we determine its component frequencies?
+We may try to weigh points on our function so that the points of peaks of separate sound
+waves are given a greater weight 
+`;
+
+const exampleTransform = DFT([1,2,3].map(x => Math.cos(x)));
+const exampleInverse = inverseDFT(example)
+console.log(DFT([1, 2, 3].map(x => Math.cos(x))));
 
 class RootComponent extends React.Component {
   constructor() {
@@ -32,19 +62,25 @@ class RootComponent extends React.Component {
   render() {
     const {loading, data} = this.state;
     if (loading) {
-      return <h1>LOADING</h1>;
+      return <h1 class="header">LOADING</h1>;
     }
     return (
-      <div className="relative">
-        <h1> Hello Explainable!</h1>
-        <div>{`The example data was loaded! There are ${data.length} rows`}</div>
+      <div className="container">
+        <h1 className="header">Analyzing Sound Waves with the Discrete Fourier Transform</h1>
+        <div>{paragraph1}</div>
+        <div>{paragraph2}</div>
+        <div>{paragraph3}</div>
+        <div>{paragraph4}</div>
         <ExampleChart data={data}/>
-        <div>{longBlock}</div>
-        <ExampleChart data={data}/>
-        <div>{longBlock}</div>
       </div>
     );
   }
 }
 RootComponent.displayName = 'RootComponent';
 export default RootComponent;
+
+/*
+<div>{`The example data was loaded! There are ${data.length} rows`}</div>
+<ExampleChart data={data}/>
+<div>{longBlock}</div>
+*/
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,55 @@
 // you can put util functions here if you want
+export function DFT(sampling) {
+	const N = sampling.length; // Number of sampling points
+	const indices = [...new Array(N)].map((d, i) => i);
+
+	console.log(N);
+	console.log('Hello!');
+	console.log(indices);
+	const transform = indices.map(n => {
+		let cosHolder = sampling.reduce((acc, d, indx) => {
+			return acc + d*Math.cos(2*Math.PI*indx*n/N);
+		});
+		let sinHolder = sampling.reduce((acc, d, indx) => {
+			return acc + d*Math.sin(2*Math.PI*indx*n/N);
+		});
+		return {'re': cosHolder, 'im': sinHolder};
+	});
+
+	return transform;
+}
+
+export function inverseDFT(frequencySampling) {
+	// the inverse is given by y_n = (1/N)*sum_{k = 0}^{N-1}x_k*e^{+2*pi*n*k/N}
+	const N = sampling.length;
+	const indices = [...new Array(N)].map((d, i) => i);
+	const inverseTransform = indices.map(n => {
+		let reHolder = sampling.reduce((acc, d, indx) => {
+			return acc + (d['re']*cos(2*Math.PI*indx*n/N) - d['im']*sin(2*Math.PI*indx*n/N));
+		});
+		let imHolder = sampling.reduce((acc, d, indx) => {
+			return acc + (d['re']*sin(2*Math.PI*indx*n/N) + d['im']*cos(2*Math.PI*indx*n/N));
+		});
+		return {'re': reHolder, 'im', imHolder};
+	});
+
+	return inverseTransform;
+
+//	const samplingNew = sampling.map(x => x / N);
+//	let inverseTransform = DFT(samplingNew);
+//	inverseTransform = inverseTransform.map(d => {'re': d['cos'], 'im': -1*d['sin']});
+}
+
+function buildWave(A, k) {
+	return (x => A*Math.sin(k*x));
+}
+
+export function polarPlot(f) {
+	// Calculate cartesian points from polar coordinates given
+	// radius = f(theta)
+	thetaPoints = [...new Array(100)];
+	xPoints = thetaPoints.map(t => f(t)*Math.cos(2*Math.PI*t/100));
+	yPoints = thetaPoints.map(t => f(t)*Math.sin(2*Math.PI*t/100));
+
+	return xPoints.map((x, indx) => (x, yPoints[indx]));
+}
