373fac216e7e88a2cd3248b1ffad5c6394451827
w4rner <w4rner@laurencewarner.com>
2019-06-06 13:20:10 -0500

    trying patrick vis 1

--- src/utils.js
+++ src/utils.js
@@ -49,6 +49,41 @@ export function fft2(X) {
 }
 */
 
+export function first(){
+    let xs = []
+
+    for(var i = 0; i < 200 * Math.PI; i+= .01){
+        xs.push(i);
+    }
+
+    let t = 0;
+
+    let A = 20;
+    let f = 1/10;
+    let height = 200;
+
+    $("#slider").on("input", function() {
+        A = $(this).val();
+    });
+
+    function animate(){
+        let points = xs.map(x => {
+            let y = A * Math.sin((x + t) * f);
+            return[x,y + height];
+        });
+
+        let path = "M" + points.map(p => {
+            return p[0] + "," + p[1];
+        }).join('L')
+
+        document.querySelector("#first").setAttribute("d", path);
+
+        t += 1;
+        requestAnimationFrame(animate);
+
+    };
+    animate();
+}
 
 export function inverseDFT(frequencySampling) {
   // the inverse is given by y_n = (1/N)*sum_{k = 0}^{N-1}x_k*e^{+2*pi*n*k/N}
